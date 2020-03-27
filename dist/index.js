b74aaa79774f3cbc13b8f2fab92a07d4c9c5531a
Gaurang Shah <gaurangnshah@gmail.com>
2020-03-27 12:27:10 -0400

    bb

--- dist/index.js
+++ dist/index.js
@@ -3500,27 +3500,31 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(__webpack_require__(470));
-const github_1 = __importDefault(__webpack_require__(469));
+const core = __importStar(__webpack_require__(470));
+const github = __importStar(__webpack_require__(469));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             // const token = core.getInput('github-token', {required: true})
-            const { pull_request: pr } = github_1.default.context.payload;
+            const { pull_request: pr } = github.context.payload;
             if (!pr) {
                 throw new Error('Event payload missing `pull_request`');
             }
-            // for (const key in pr) {
-            //   // console.log(key)
-            // }
+            for (const key in pr) {
+                core.debug(key);
+            }
             // const client = new github.GitHub(token)
         }
         catch (error) {
-            core_1.default.setFailed(error.message);
+            core.setFailed(error.message);
         }
     });
 }
