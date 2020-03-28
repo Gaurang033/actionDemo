c1fbb7f5154930a9514edc17c26b3eae0150a44d
Gaurang Shah <gaurangnshah@gmail.com>
2020-03-28 19:32:22 -0400

    aa

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,9 @@ async function run(): Promise<void> {
     }
 
     core.info('total number of commits are: ${pr["commits"]}')
+    if(pr["commits"] > 1){
+      core.setFailed("total number of commits are greater than 1, please squash your commits")
+    }
     // const client = new github.GitHub(token)
   } catch (error) {
     core.setFailed(error.message)
