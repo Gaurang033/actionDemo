33c04123e63c99ddb8cf5beea7631136ca41e3f0
Gaurang Shah <gaurangnshah@gmail.com>
2020-03-28 19:43:02 -0400

    aa

--- dist/index.js
+++ dist/index.js
@@ -3519,9 +3519,9 @@ function run() {
             if (!pr) {
                 throw new Error('Event payload missing `pull_request`');
             }
-            core.info('total number of commits are: ${pr["commits"]}');
-            if (pr["commits"] > 1) {
-                core.setFailed("total number of commits are greater than 1, please squash your commits");
+            core.info("total number of commits are: ${pr['commits']}");
+            if (pr['commits'] > 1) {
+                core.setFailed('total number of commits are greater than 1, please squash your commits');
             }
             // const client = new github.GitHub(token)
         }
